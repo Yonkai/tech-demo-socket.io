c4d69eccca4bb27a1c31e22edbfe9695cf51fd8c
Frank <fgunsch.webdev@gmail.com>
2018-08-11 19:00:53 -0400

    "More tutorial information."

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+    console.log('a user connected');
+  });
+  
 app.get('/other', function(req, res){
     res.send('<h1>Alternative world</h1>');
   });
