8cb1899fccbfde325e49caa5f8fee0bf2105a070
Frank <fgunsch.webdev@gmail.com>
2018-08-11 19:48:55 -0400

    "Finished Tutorial from socket.io docs"

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -7,6 +9,17 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+      });
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+      });
   });
   
 app.get('/other', function(req, res){
